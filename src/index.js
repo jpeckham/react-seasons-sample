d53276054172a6704adfd5a09fd230be8d108c36
James Peckham <jdpeckham@gmail.com>
2019-03-03 10:25:01 -0600

    finished course

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    state = { lat: null, long: null, errorMessage: null };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor(props) {
+        console.log('constructor');
+        super(props);
+    }
+    renderContent() {
+        if (this.state && this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            )
+        }
+        else if (this.state && this.state.lat && !this.state.errorMessage)
+            return <SeasonDisplay lat={this.state.lat} />;
+        else
+            return <Spinner text="Waiting for location" />;
+    }
+    render() {
+        console.log('render');
+        return (
+            <div className="ui">
+                {this.renderContent()}
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState({ lat: position.coords.latitude });
+                this.setState({ long: position.coords.longitude });
+
+            },
+            (err) => {
+                console.log(err);
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
